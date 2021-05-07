f62cabfdcd748223aa96e844805e86aaccb6f15c
Bryan Gonzalez <bryangonzalezm@outlook.com>
2021-05-06 19:45:30 -0500

    Creando elementos estáticos

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,10 +1,19 @@
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0;  //Auntoincrement
     String name;
     String speciality;
+
+    Doctor(){
+        id++;
+    }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showID(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import static ui.UImenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Horacio Quiroga";
-
         myDoctor.showName();
+        myDoctor.showID();
+
+        Doctor myDoctor2 = new Doctor();
+        myDoctor2.name = "Bryan Gonzalez";
+        myDoctor2.showName();
+        myDoctor2.showID();
+
+        showMenu();
     }
 }
